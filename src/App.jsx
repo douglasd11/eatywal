f07ffa3b6252cb9f0e9417dca0d71a2234815681
douglasd11 <dguerreroe@unicartagena.edu.co>
2024-08-24 12:07:14 -0500

    primer commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import './App.css'
+import './index.css'
+
+import '@fontsource-variable/saira';
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-8xl font-normal text-blue-400">
+        Hello world
+      </h1>
+    </>
+  )
+}
+
+export default App
